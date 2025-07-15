edd1066e878f081429e51fa59f39a188e3f5729e
Dhiego Henrique Balthazar de Sousa <dhiegobalthazar@gmail.com>
2025-07-15 19:46:30 -0300

    pronto para o download

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+    input.addEventListener("input", (event) => {
+        let value = event.target.value;
+        const regNumbers = new RegExp(/^\d+(,\d*)?$/);
+        let isNumber = regNumbers.test(value);
+        console.log(isNumber);
+        if (!isNumber) {
+            event.target.value = value.slice(0, -1);
+        }
+    });
+});
+
+function apresentarResultado(resultado){
+    resultado = String(resultado.toFixed(2)).replace(".",",");
+    const resultContainer = document.querySelector(".result-container");
+    const title = document.createElement("h2");
+    title.textContent = `Resultado dos investimentos`;
+    const totalJuros = document.createElement("p");totalJuros.textContent = `Total com juros: R$ ${resultado}`;
+    totalJuros.classList.add("total-juros");
+    resultContainer.appendChild(title);
+    resultContainer.appendChild(totalJuros);
+}
+
+const btnCalcular = document.querySelector("#calcular");
+btnCalcular.addEventListener("click", (event) => {
+    event.preventDefault();
+    //Altere a partir daqui
+    
+    //Não apague esta função
+    apresentarResultado(resultado);
+});
