f47125da25dba4e9421c2d98ead50d8e51db885e
Dhiego Henrique Balthazar de Sousa <dhiegobalthazar@gmail.com>
2025-07-17 13:36:52 -0300

    Calculadora Completa

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,8 +12,18 @@ function apresentarResultado(resultado){
 const btnCalcular = document.querySelector("#calcular");
 btnCalcular.addEventListener("click", (event) => {
     event.preventDefault();
+    //M=(C(1+i)^t)+aporte*t-1
+
     //Altere a partir daqui
+    const valorInicial = Number(document.querySelector("#valor-inicial").value).toFixed(2);
+    const aporte = Number(document.querySelector("#aporte").value).toFixed(2);
+    const taxaMensal = Number(document.querySelector("#taxa-mensal").value).toFixed(2);
+    const tempo = Number(document.querySelector("#tempo").value).toFixed(2);
+
+    let resultado = valorInicial*((1+taxaMensal/100)**tempo) + aporte*(tempo-1);
     
-    //Não apague esta função
+    
+    
+    //Não apague a linha abaixo
     apresentarResultado(resultado);
 });
